19559a82337d1fab7dec3809c96c1d4d1a0e09fc
DaVinciB <okishteam@gmail.com>
2026-05-10 19:39:15 -0400

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -11,14 +11,17 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    console.log('Deploying commands...');
+    console.log('Deploying guild commands...');
 
     await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID),
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.SERVER_ID
+      ),
       { body: commands },
     );
 
-    console.log('Commands deployed successfully.');
+    console.log('Guild commands deployed successfully.');
   } catch (error) {
     console.error(error);
   }
