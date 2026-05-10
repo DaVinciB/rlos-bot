13eda91986cb32cbed42792cac709d62b1e73908
DaVinciB <okishteam@gmail.com>
2026-05-10 02:11:59 -0400

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,20 +1,24 @@
 const { REST, Routes } = require('discord.js');
 require('dotenv').config();
 
-const commands = [];
+const signupCommand = require('./commands/signup');
+
+const commands = [
+  signupCommand.data.toJSON(),
+];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    console.log('Started refreshing application commands.');
+    console.log('Deploying commands...');
 
     await rest.put(
       Routes.applicationCommands(process.env.CLIENT_ID),
       { body: commands },
     );
 
-    console.log('Successfully reloaded application commands.');
+    console.log('Commands deployed successfully.');
   } catch (error) {
     console.error(error);
   }
