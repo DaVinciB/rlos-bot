962ff14e71481c3b19e20796465923aeb82b6522
DaVinciB <okishteam@gmail.com>
2026-05-17 00:05:14 -0400

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -5,29 +5,37 @@ const signupCommand = require('./commands/signup');
 const playersCommand = require('./commands/players');
 const teamsCommand = require('./commands/teams');
 const divisionsCommand = require('./commands/divisions');
+const divisionTeamsCommand = require('./commands/divisionteams');
 
 const commands = [
   signupCommand.data.toJSON(),
   playersCommand.data.toJSON(),
   teamsCommand.data.toJSON(),
   divisionsCommand.data.toJSON(),
+  divisionTeamsCommand.data.toJSON(),
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
+
     console.log('Deploying guild commands...');
 
     await rest.put(
+
       Routes.applicationGuildCommands(
         process.env.CLIENT_ID,
         process.env.SERVER_ID
       ),
+
       { body: commands },
     );
 
-    console.log('Guild commands deployed successfully.');
+    console.log(
+      'Guild commands deployed successfully.'
+    );
+
   } catch (error) {
     console.error(error);
   }
